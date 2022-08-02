092bbd75cff88bdcde4529b34eef763f4c1c9f0a
ibtisam <iuhmirza@gmail.com>
2022-08-02 17:42:00 +0100

    Refactor code

--- scripts.js
+++ scripts.js
@@ -2,21 +2,25 @@ const container = document.querySelector('.container')
 
 function generateGrid() {
     gridSize = parseInt(prompt("Enter the length of the sides of the grid: "))
-    while ((typeof gridSize !== 'number') || (gridSize < 1) || (gridSize > 10)) {
-        gridSize = parseInt(prompt("Enter the length of the sides of the grid: Min = 1 and Max = 10"))
+    while ((typeof gridSize !== 'number') || (gridSize < 16) || (gridSize > 64)) {
+        gridSize = parseInt(prompt("Enter the length of the sides of the grid: Min = 16 and Max = 64"))
     }
+
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+
     for(let i = 0; i < gridSize; i++) {
-        let yAxis = document.createElement('div')
-        yAxis.classList.add('flex')
-        yAxis.classList.add('container')
-        yAxis.classList.add('column')
+        let columns = document.createElement('div')
+        columns.classList.add('flex', 'column')
         for(let j = 0; j < gridSize; j++) {
             let card = document.createElement('div')
             card.classList.add('pixel')
-            yAxis.appendChild(card)
+            columns.appendChild(card)
         }
-        container.appendChild(yAxis)
+        container.appendChild(columns)
     }
+
     const pixel = document.querySelectorAll('.pixel')
     pixel.forEach(item => {
         item.addEventListener('click', () => {

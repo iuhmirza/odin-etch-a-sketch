188489e429fde8cc835deb14842737cc76285e5a
ibtisam <iuhmirza@gmail.com>
2022-08-02 16:46:36 +0100

    Add resizable grid

--- scripts.js
+++ scripts.js
@@ -1,23 +1,30 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.container')
 
-for(let i = 0; i < 10; i++) {
-    let yAxis = document.createElement('div')
-    yAxis.classList.add('flex')
-    yAxis.classList.add('container')
-    yAxis.classList.add('y')
-    for(let j = 0; j < 10; j++) {
-        let card = document.createElement('div');
-        card.classList.add('pixel')
-        yAxis.appendChild(card)
+function generateGrid() {
+    gridSize = parseInt(prompt("Enter the length of the sides of the grid: "))
+    while ((typeof gridSize !== 'number') || (gridSize < 1) || (gridSize > 10)) {
+        gridSize = parseInt(prompt("Enter the length of the sides of the grid: Min = 1 and Max = 10"))
     }
-    container.appendChild(yAxis)
+    for(let i = 0; i < gridSize; i++) {
+        let yAxis = document.createElement('div')
+        yAxis.classList.add('flex')
+        yAxis.classList.add('container')
+        yAxis.classList.add('column')
+        for(let j = 0; j < gridSize; j++) {
+            let card = document.createElement('div')
+            card.classList.add('pixel')
+            yAxis.appendChild(card)
+        }
+        container.appendChild(yAxis)
+    }
+    const pixel = document.querySelectorAll('.pixel')
+    pixel.forEach(item => {
+        item.addEventListener('click', () => {
+            item.classList.toggle('painted')
+        })
+    })
+    
+    
 }
 
-const pixel = document.querySelectorAll('.pixel');
-console.log(pixel);
 
-pixel.forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.toggle('painted')
-    })
-})

084ed3483bf616ef08eae1db6470f231ed88a868
ibtisam <iuhmirza@gmail.com>
2022-08-02 16:26:54 +0100

    Generate grid with js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
+const container = document.querySelector('.container');
+
+for(let i = 0; i < 10; i++) {
+    let yAxis = document.createElement('div')
+    yAxis.classList.add('flex')
+    yAxis.classList.add('container')
+    yAxis.classList.add('y')
+    for(let j = 0; j < 10; j++) {
+        let card = document.createElement('div');
+        card.classList.add('pixel')
+        yAxis.appendChild(card)
+    }
+    container.appendChild(yAxis)
+}
 
 const pixel = document.querySelectorAll('.pixel');
 console.log(pixel);
 
 pixel.forEach(item => {
     item.addEventListener('click', () => {
-        item.classList.add('painted')
+        item.classList.toggle('painted')
     })
 })
